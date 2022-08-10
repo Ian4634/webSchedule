987b940e9b66023a9ced50c85975e3bd7a072a62
Ian <flyingiancheng@gmail.com>
2022-08-11 00:01:09 +0800

    scheduleBoard initilized

--- webSchedule/scheduleBoard/models.py
+++ webSchedule/scheduleBoard/models.py
@@ -5,3 +5,4 @@ class Lesson(models.Model):
     time = models.CharField(max_length=200)
     notes = models.CharField(max_length = 500)
     instructors = models.CharField(max_length = 200)
+    student_names = models.CharField(max_length = 500, default='no info')
--- webSchedule/scheduleBoard/views.py
+++ webSchedule/scheduleBoard/views.py
@@ -13,6 +13,10 @@ def process(request):
         time = request.POST['time']
         notes = request.POST['notes']
         instructors = request.POST['instructors']
-        Lesson.objects.create(time = time, notes = notes, instructors = instructors).save()
+        student_names = request.POST['student_names']
+        if student_names != '':
+            Lesson.objects.create(time = time, notes = notes, instructors = instructors,student_names = student_names).save()
+        else:
+            Lesson.objects.create(time = time, notes = notes, instructors = instructors).save()
        
     return redirect('http://localhost:8000/scheduleBoard/')
