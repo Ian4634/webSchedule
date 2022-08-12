87346ff4ecb49fb9fcf38482b424ae29cb4f17ca
Ian <flyingiancheng@gmail.com>
2022-08-12 08:28:02 +0800

    'haha'

--- webSchedule/StudentDatabase/models.py
+++ webSchedule/StudentDatabase/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    shoe = models.DecimalField(max_digits=5, decimal_places=1)
+    name = models.CharField(max_length = 50)
+    shoe = models.CharField(max_length=10, blank=True)
     board = models.CharField(max_length=100, blank=True)
     stance = models.CharField(max_length=200, blank=True)
+    bindingSize = models.CharField(max_length = 200, blank=True)
--- webSchedule/StudentDatabase/views.py
+++ webSchedule/StudentDatabase/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Student
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def home(request):
     return render(request, 'StudentDatabase.html')
+
+def addStudent(request):
+    return render(request, 'add_studentData.html')
+def process(request):
+    name = request.POST['student_name']
+    shoe = request.POST['shoe']
+    board = request.POST['board']
+    stance = request.POST['stance']
+    bindingSize = request.POST['bindingSize']
+    Student.objects.create(
+        name = name,
+        shoe = shoe,
+        board = board,
+        stance = stance,
+        bindingSize = bindingSize
+    ).save()
+    return HttpResponseRedirect(reverse('home'))
